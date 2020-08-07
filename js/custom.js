f2e78594ebee8160320c3abc481fba1622547e2f
frontendvault <manoj07.pathania@gmail.com>
2020-08-07 19:31:57 +0530

    alert close active egular and all buttons in checkbox show hide in items modal

--- js/custom.js
+++ js/custom.js
@@ -5,9 +5,4 @@ $(function () {
   });
 });
 
-// $(function() {
-//     $('#colorselector').change(function(){
-//         $('.colors').hide();
-//         $('#' + $(this).val()).show();
-//     });
-// });
+// $('.alert').alert();
