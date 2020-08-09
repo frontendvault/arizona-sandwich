76df9d1d9d9459ab31102fa59efc89cf28fff1f9
frontendvault <manoj07.pathania@gmail.com>
2020-08-09 05:47:51 +0530

    order later page starter

--- js/custom.js
+++ js/custom.js
@@ -7,8 +7,37 @@ $(function () {
     theme: 'dark-thin',
   });
 });
-$(document).ready(function () {
-  $('.toggle-map').click(function () {
-    $('.toggle-map-container').toggle();
+jQuery(document).ready(function ($) {
+  $('.slider').slick({
+    dots: false,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 8,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    arrows: true,
+    prevArrow: '<button class="slide-arrow prev-arrow"></button>',
+    nextArrow: '<button class="slide-arrow next-arrow"></button>',
+    responsive: [
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 400,
+        settings: {
+          arrows: true,
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
   });
+});
+$('.toggle-map').click(function () {
+  $('.toggle-map-container').toggle();
 });
